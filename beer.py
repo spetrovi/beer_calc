2e6eefe5f679302a9c2fef1380440b805349761f
Samuel Petrovic <spetrovi@redhat.com>
2018-03-01 17:24:13 +0100

    Volume computing fixed new beer added
    Signed-off-by: Samuel Petrovic <spetrovi@redhat.com>

--- beer.py
+++ beer.py
@@ -2,6 +2,44 @@
 #Karamelovy slad: 600g, 76.8%
 #Pale ale: 5000g, 81.5%
 
+slady = [('Psenicny', 2700, 82.3),\
+('PaleAle', 1800, 81.5)]
+
+total_cukor = 0
+for item in slady:
+        cukor = item[1]/100*item[2]
+        print item[0] + ': ' +str(cukor)+'g'
+        total_cukor += cukor
+print '\n'
+print 'Total cukor: ' + str(total_cukor) + 'g'
+
+print 'V 20l vody: ' + str(total_cukor/20)
+print '\n'
+
+vyska_vo_fermentacke = 2.5
+
+vysledny_objem = 3.14 * 1.6 * 1.6 * vyska_vo_fermentacke
+print 'Vysledny objem: ' + str(vysledny_objem) + 'l'
+
+ucinnost_pre_objem = total_cukor/vysledny_objem
+print '100% ucinnost pre vysledny objem: ' + str(ucinnost_pre_objem)
+
+vysledna_hustota = 100
+vysledna_ucinnost = vysledna_hustota/ucinnost_pre_objem 
+print 'Vysledna ucinnotst: ' + str(vysledna_ucinnost*100)+'%\n\n'
+
+vysledny_cukor = vysledna_hustota*vysledny_objem
+print 'Vysledny cukor: ' +str(vysledny_cukor)+'g'
+
+
+
+
+
+
+
+
+
+
 slady = [('Carafa2', 300, 65),\
 ('Karamel', 600, 76.8),\
 ('PaleAle', 5000, 81.5)]
@@ -19,7 +57,7 @@ print '\n'
 
 vyska_vo_fermentacke = 2.2
 
-vysledny_objem = 3.14 * 3.2 * vyska_vo_fermentacke
+vysledny_objem = 3.14 * 1.6 * 1.6 * vyska_vo_fermentacke
 print 'Vysledny objem: ' + str(vysledny_objem) + 'l'
 
 ucinnost_pre_objem = total_cukor/vysledny_objem
@@ -53,7 +91,7 @@ print '\n'
 
 vyska_vo_fermentacke = 1.5
 
-vysledny_objem = 3.14 * 3.2 * vyska_vo_fermentacke
+vysledny_objem = 3.14 * 1.6 * 1.6 * vyska_vo_fermentacke
 print 'Vysledny objem: ' + str(vysledny_objem) + 'l'
 
 ucinnost_pre_objem = total_cukor/vysledny_objem
