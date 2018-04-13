be6f2c997a6cc93e6c35848e1a93e836ab8eaa8c
Samuel Petrovic <spetrovi@redhat.com>
2018-04-13 12:38:34 +0200

    New beer Dormiens
    Signed-off-by: Samuel Petrovic <spetrovi@redhat.com>

--- beer.py
+++ beer.py
@@ -1,6 +1,3 @@
-#Carafa 2: 300g, 65%
-#Karamelovy slad: 600g, 76.8%
-#Pale ale: 5000g, 81.5%
 
 slady = [('Psenicny', 2700, 82.3),\
 ('PaleAle', 1800, 81.5)]
@@ -100,5 +97,37 @@ print '100% ucinnost pre vysledny objem: ' + str(ucinnost_pre_objem)
 vysledna_hustota = 124
 vysledna_ucinnost = vysledna_hustota/ucinnost_pre_objem 
 print 'Vysledna ucinnotst: ' + str(vysledna_ucinnost*100)+'%'
+print '\n'
+print '\n'
+print '\n'
+print '\n'
+print '\n'
+slady = [('Simpsons', 400, 65),\
+('PaleAle', 3600, 81.5)]
+
+total_cukor = 0
+for item in slady:
+        cukor = item[1]/100*item[2]
+        print item[0] + ': ' +str(cukor)+'g'
+        total_cukor += cukor
+print 'Total cukor: ' + str(total_cukor) + 'g'
+print 'V 20l vody: ' + str(total_cukor/20)
+print '\n'
+
+vyska_vo_fermentacke = 2.8
+
+vysledny_objem = 3.14 * 1.6 * 1.6 * vyska_vo_fermentacke
+print 'Vysledny objem: ' + str(vysledny_objem) + 'l'
+
+ucinnost_pre_objem = total_cukor/vysledny_objem
+#print '100% ucinnost pre vysledny objem: ' + str(ucinnost_pre_objem)
+
+vysledna_hustota = 100
+print 'Vysledna hustota: ' + str(vysledna_hustota)
+vysledna_ucinnost = vysledna_hustota/ucinnost_pre_objem 
+print 'Vysledna ucinnost: ' + str(vysledna_ucinnost*100)+'%'
+
+vysledny_cukor = vysledna_hustota*vysledny_objem
+print 'Vysledny cukor: ' +str(vysledny_cukor)+'g'
 
 
