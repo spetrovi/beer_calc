1474d45a1d94d79a814c069bfecc452d298f101b
Samuel Petrovic <spetrovi@redhat.com>
2018-10-05 10:38:40 +0200

    Pumpkin Ale
    Signed-off-by: Samuel Petrovic <spetrovi@redhat.com>

--- beer.py
+++ beer.py
@@ -97,16 +97,10 @@ objem = 24
 hustota = 120
 calculate2(nazov, slady, objem, hustota)
 
-nazov = 'Vajano'
-slady = [('PaleAle', 5000, 81.5)]
-objem = 24
-hustota = 120
-calculate2(nazov, slady, objem, hustota)
-
 
 nazov = 'Pumpkin'
 slady = [('PaleAle', 5000, 81.5), ('Carabelge', 300, 74)]
-objem = 24
-hustota = 126
+objem = 26
+hustota = 100
 calculate2(nazov, slady, objem, hustota)
 
